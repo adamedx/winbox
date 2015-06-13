d08ade52cd6b395a590b05642013c5fed68c71ee
adamedx <adamed@getchef.com>
2015-06-13 06:37:05 -0700

    Version bump for previous change

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adamedx'
 license          'Apache 2.0'
 description      'Configures tools for Chef development on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.12'
+version          '0.1.13'
 depends          'git'
 depends          'windows'
 
