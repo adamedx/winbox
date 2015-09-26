86cd89c16a39ce24b48a9af7af97d04db9b5ebbb
Adam Edwards <adamed@opscode.com>
2015-09-26 01:52:29 -0700

    Update Windows cookbook dep to 1.38.2 to fix #17

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'adamedx'
 license          'Apache 2.0'
 description      'Configures tools for development on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.90'
+version          '0.1.91'
 depends          'git'
-depends          'windows'
+depends          'windows', '>= 1.38.2'
 
 supports         'windows'
 
