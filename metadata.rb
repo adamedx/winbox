50cd6d4cd7155122c5ccb344079acb9d27d43c4b
adamedx <adamed@getchef.com>
2015-09-26 22:59:27 -0700

    Update metadata with source, issues urls

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,14 @@ maintainer_email 'adamedx'
 license          'Apache 2.0'
 description      'Configures tools for development on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.100'
+version          '0.9.101'
 
 depends          'git'
 depends          'windows'
 
 supports         'windows'
+
+source_url 'https://github.com/adamedx/winbox' if respond_to?(:source_url)
+issues_url 'https://github.com/adamedx/winbox/issues' if respond_to?(:issues_url)
 
 
